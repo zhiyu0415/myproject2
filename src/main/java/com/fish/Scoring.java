80d46251ada3c4bb8b2e10ce3bebd58ec3a03297
fish <yvonneli0415@gmail.com>
2022-03-04 16:08:46 +0800

    Scoring

---
+++ src/main/java/com/fish/Scoring.java
@@ -0,0 +1,16 @@
+package com.fish;
+
+import javax.xml.namespace.QName;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student tony=new Student();
+        tony.name = "Tony";
+        tony.english = 98;
+        tony.math = 100;
+        Student andy=new Student();
+        andy.name = "Andy";
+        andy.english = 86;
+        andy.math = 58;
+    }
+}
---
+++ src/main/java/com/fish/Student.java
@@ -0,0 +1,7 @@
+package com.fish;
+
+public class Student {
+    String name;
+    int english;
+    int math;
+}
