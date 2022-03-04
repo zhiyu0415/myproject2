5f4bab4ec7c8b300337ab22dc4f63433eb62d069
fish <yvonneli0415@gmail.com>
2022-03-04 16:11:55 +0800

    Scoring

--- src/main/java/com/fish/Scoring.java
+++ src/main/java/com/fish/Scoring.java
@@ -12,5 +12,17 @@ public class Scoring {
         andy.name = "Andy";
         andy.english = 86;
         andy.math = 58;
+        Student anne=new Student();
+        anne.name = "Anne";
+        anne.english = 73;
+        anne.math = 63;
+        Student hank=new Student();
+        hank.name = "Hank";
+        hank.english = 60;
+        hank.math = 20;
+        Student tina=new Student();
+        tina.name = "Tina";
+        tina.english = 59;
+        tina.math = 77;
     }
 }
