4f668ff822a8822fc5ad72bf7b560333d7170f79
User <yvonneli0415@gmail.com>
2022-03-18 16:12:50 +0800

    GoldenCustomer

--- src/main/java/com/fish/GoldenCustomer.java
+++ src/main/java/com/fish/GoldenCustomer.java
@@ -1,7 +1,9 @@
 package com.fish;
 
-public class GoldenCustomer extends Customer{
+public class GoldenCustomer extends SilverCustomer{
     public GoldenCustomer(String customer, int spent) {
         super(customer, spent);
+        off=0.2f;
     }
+
 }
--- src/main/java/com/fish/Tester.java
+++ src/main/java/com/fish/Tester.java
@@ -2,11 +2,13 @@ package com.fish;
 
 public class Tester {
     public static void main(String[] args) {
-        Customer c1= new Customer("0001",1200);
-        Customer c2= new Customer("0002",800);
-        Customer c3= new SilverCustomer("0003",2000);
+        Customer c1 = new Customer("0001",1200);
+        Customer c2 = new Customer("0002",800);
+        Customer c3 = new SilverCustomer("0003",2000);
+        Customer c4 = new GoldenCustomer("0004",5000);
         c1.print();
         c2.print();
         c3.print();
+        c4.print();
     }
 }
