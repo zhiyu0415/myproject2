d3a478e7748749dc4833274ed432f93efe8c7d40
User <yvonneli0415@gmail.com>
2022-03-18 16:14:39 +0800

    GoldenCustomer

--- src/main/java/com/fish/GoldenCustomer.java
+++ src/main/java/com/fish/GoldenCustomer.java
@@ -1,9 +1,9 @@
 package com.fish;
 
-public class GoldenCustomer extends SilverCustomer{
+public class GoldenCustomer extends SilverCustomer{//SilverCustomer的比Customs重複性更高
     public GoldenCustomer(String customer, int spent) {
         super(customer, spent);
-        off=0.2f;
+        off=0.2f;//繼承SilverCustomer的off=0.1所以在繼承建構子後改寫off的數值
     }
 
 }
