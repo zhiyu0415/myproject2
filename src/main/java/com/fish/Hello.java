e9cdd5f9813228d74ae6eac6bf61a7064190de73
User <yvonneli0415@gmail.com>
2022-04-01 16:22:49 +0800

    list

--- src/main/java/com/fish/Hello.java
+++ src/main/java/com/fish/Hello.java
@@ -1,10 +1,15 @@
 package com.fish;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello!");
         int diff = Math.abs(100-300);//取絕對值
         System.out.println(diff);
+        ArrayList<Integer>list = new ArrayList<>();
+        List<Integer> lists = new ArrayList<>();//import之後左右類別不同依舊可以使用
     }
     //static 類別層級的屬性
     //final 固定常數
--- src/main/java/com/fish/sales/Tester.java
+++ src/main/java/com/fish/sales/Tester.java
@@ -1,18 +1,32 @@
 package com.fish.sales;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tester {
     public static void main(String[] args) {
-        Customer c1 = new Customer("0001",1200);
-        Customer c2 = new Customer("0002",800);
-        Customer c3 = new SilverCustomer("0003",2000);
-        Customer c4 = new GoldenCustomer("0004",5000);
-        DiscountCustomer c5 = new DiscountCustomer("0005",900);
-        PlatinumCustomer c6 = new PlatinumCustomer("0006",2000);
+        List<Customer>customers = new ArrayList<>();
+        customers.add(new Customer("0001",1200));
+        customers.add(new Customer("0002",800));
+        customers.add(new SilverCustomer("0003",2000));
+        customers.add(new GoldenCustomer("0004",5000));
+        customers.add(new DiscountCustomer("0005",900));
+        customers.add(new PlatinumCustomer("0006",2000));
+        for (int i = 0; i < 6; i++) {
+            customers.get(i).print();
+        }
+
+       /* Customer c1 = ;
+        Customer c2 = ;
+        Customer c3 = ;
+        Customer c4 = ;
+        DiscountCustomer c5 = ;
+        PlatinumCustomer c6 = ;
         c1.print();
         c2.print();
         c3.print();
         c4.print();
         c5.print();
-        c6.print();
+        c6.print();*/
     }
 }
