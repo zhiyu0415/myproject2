aa80bf2b476146d9ea76df9d568cb38050a01748
fish <yvonneli0415@gmail.com>
2022-05-20 15:32:54 +0800

    layout

--- src/main/java/com/fish/ui/MyWin.java
+++ src/main/java/com/fish/ui/MyWin.java
@@ -1,13 +1,30 @@
 package com.fish.ui;
 
 import javax.swing.*;//找這個資料夾
+import java.awt.*;
 
 public class MyWin {
     //swing,SWT,JavaFX視窗類別庫
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setVisible(true);
+        frame.setSize(600,400);
+        frame.setLocation(300,200);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//關掉視窗的同時停止程式
+        //add component加元件
+        JButton button = new JButton("OK");
+        //Layout
+        //frame.setLayout(new BorderLayout());//常用
+        frame.setLayout(new FlowLayout());//流水性，向右排序
+        frame.add(button);//沒有特別說明會直接放在C位，並占滿畫面
+        frame.add(new JButton("testing"));
+        frame.add(new JButton("testing"));
+        frame.add(new JButton("testing"));
+        frame.add(new JButton("testing"));
+        frame.add(new JButton("testing"));
+        frame.add(new JButton("testing"));
+        frame.add(new JButton("testing"));
 
+        frame.setVisible(true);
         System.out.println("END?");
     }
 
