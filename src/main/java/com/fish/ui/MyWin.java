baa6fb73317c5231ab0e52c4dbd506c2e4c6780b
fish <yvonneli0415@gmail.com>
2022-05-20 14:39:01 +0800

    UI

---
+++ src/main/java/com/fish/ui/MyWin.java
@@ -0,0 +1,15 @@
+package com.fish.ui;
+
+import javax.swing.*;//找這個資料夾
+
+public class MyWin {
+    //swing,SWT,JavaFX視窗類別庫
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+
+        System.out.println("END?");
+    }
+
+
+}
