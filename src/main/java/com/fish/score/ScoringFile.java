d753f141a8768692b1f7a6a486e6131cad2a1f59
fish <yvonneli0415@gmail.com>
2022-06-17 15:33:08 +0800

    JAVA ScoringFile

---
+++ src/main/java/com/fish/score/ScoringFile.java
@@ -0,0 +1,9 @@
+package com.fish.score;
+
+import java.io.FileReader;
+
+public class ScoringFile {
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader("scores.txt");
+    }
+}
