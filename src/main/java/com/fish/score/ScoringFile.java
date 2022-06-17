8ab36b1d9edff51f92f0585cce649f0cf5d7c1dd
fish <yvonneli0415@gmail.com>
2022-06-17 16:34:49 +0800

    JAVA fileReader

--- src/main/java/com/fish/score/ScoringFile.java
+++ src/main/java/com/fish/score/ScoringFile.java
@@ -1,5 +1,6 @@
 package com.fish.score;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,13 +9,31 @@ public class ScoringFile {
     public static void main(String[] args) {
         try {
             FileReader fileReader = new FileReader("scores.txt");//throws 執行catch 接著執行try以外的資訊
+            BufferedReader reader = new BufferedReader(fileReader);
+            //Jack,88,60
+            String line = reader.readLine();
+            while(line != null){
+                //System.out.println(line);
+                String[] token = line.split(",");
+                String name = token[0];
+                int english = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Student student = new Student(name,english,math);
+                student.print();
+                line = reader.readLine();
+            }
             /*try{
                 int data = fileReader.read();
             }catch (IOException e){
                 System.out.println("資料讀取失敗");
             }*/
-            int data = fileReader.read();
-            System.out.println(data);
+            /*int data = fileReader.read();
+            //System.out.println(data);//無資料之後變成-1
+            while (data != -1){
+                System.out.print((char) data);
+                data = fileReader.read();
+            }
+            System.out.println();*/
         } catch (FileNotFoundException e) {
             System.out.println("檔案讀取失敗");
         } catch (IOException e) {
