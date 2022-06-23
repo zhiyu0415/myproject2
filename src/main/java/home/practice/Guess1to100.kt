ac2ba7b0b3dcce0362f6285799cd8684ba35da2f
fish <yvonneli0415@gmail.com>
2022-06-23 11:49:54 +0800

    Kotlin Guess1to100 fail

---
+++ src/main/java/home/practice/Guess1to100.kt
@@ -0,0 +1,16 @@
+package home.practice
+
+import java.util.Scanner
+import kotlin.random.Random
+
+class Guess1to100 {
+fun main(){
+    val secret = Random.nextInt(100)+1
+    println(secret)
+    println("Please enter your number 1 to 100")
+
+
+
+    }
+
+}
